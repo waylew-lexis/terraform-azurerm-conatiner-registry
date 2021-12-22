ee90bef2be2a6ab61a27a88d8b96d7b8b91a5ba6
waylew-lexis <86667349+waylew-lexis@users.noreply.github.com>
2021-12-22 13:38:35 -0500

    updating

--- azure-container-registry/main.tf
+++
@@ -1,7 +0,0 @@
-
-resource "azurerm_container_registry" "registry" {
-  location            = var.location
-  name                = var.name
-  resource_group_name = var.resource_group_name
-}
-
--- azure-container-registry/outputs.tf
+++
@@ -1,7 +0,0 @@
-
-output "id" {
-  value       = azurerm_container_registry.registry.id
-  description = "The id of the registry"
-  sensitive   = false
-}
-
